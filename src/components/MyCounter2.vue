be79f69f7e5044e64e45021d6f5cd0395f4b4b07
Amey Pandit <amey.pandit@sportzinteractive.com>
2024-08-26 12:54:16 +0530

    GET SET ADDED

---
+++ src/components/MyCounter2.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+      <h1>Counter 2</h1>
+      <p>Counter: {{ count }}</p>
+      <button @click="start">Increment</button>
+    </div>
+</template>
+  
+<script setup>
+  import { customRef } from 'vue';
+
+  const count = customRef((track, trigger) => {
+    let count = 0;
+
+    return {
+      get() {
+        track();
+        return count;
+      },
+      set(value) {
+        count = value;
+        trigger();
+      }
+    }
+  });
+
+  function increment() {
+    console.log("Before Increment");
+    count.value++;
+    console.log("After Increment");
+  }
+  
+</script>
+  
+  <style scoped>
+  </style>
