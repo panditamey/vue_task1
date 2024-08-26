d5b44bd30a7d4a076965266ec0208574cc5367e2
Amey Pandit <amey.pandit@sportzinteractive.com>
2024-08-26 12:32:44 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1>From app</h1>
+  <MyCounter />
+</template>
+
+<script>
+import MyCounter from './components/MyCounter.vue';
+
+export default {
+  name: 'App',
+  components: {
+    MyCounter // Updated to match the new component name
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/MyCounter.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+      <h1>Counter</h1>
+      <p>Counter: {{ count }}</p>
+      <button @click="start">Increment</button>
+    </div>
+</template>
+  
+<script setup>
+  import { ref } from 'vue';
+  let time;
+  const count = ref(0);
+  const increment = () => {
+    count.value++;
+  }
+  const start = () => {
+    time = setInterval(increment, 1000);
+  }
+
+/*
+import { onBeforeMount, onBeforeUnmount, onBeforeUpdate, onMounted, onUnmounted, onUpdated } from 'vue';
+
+  export default {
+    name: 'MyCounter' // Updated to a multi-word name
+  }
+  onBeforeMount(() => { 
+    console.log('Before Mount')
+  });
+  onMounted(() => {
+    console.log('Mounted')
+  });
+  onBeforeUpdate(() => {
+    console.log('Before Update')
+  });
+  onUpdated(() => {
+    console.log('Updated')
+  });
+  onBeforeUnmount(() => {
+    console.log('Before Unmount')
+  });
+  onUnmounted(() => {
+    console.log('Unmounted')
+  });
+*/
+</script>
+  
+  <style scoped>
+  </style>
